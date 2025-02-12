5735309093825999b2590496aa8af48f41cf4c6f
Sukhchain Singh <sukhsingh15666@gmail.com>
2025-02-12 23:16:38 +0530

    Update settings.py
    I HAVE UPDATE THE EMAIL SYSTEM

--- email_project/email_project/settings.py
+++ email_project/email_project/settings.py
@@ -124,9 +124,9 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sukhsingh15666@gmail.com'
-EMAIL_HOST_PASSWORD = 'owjl rhdc vpvt zmhv'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'sukhsingh15666@gmail.com'
+# EMAIL_HOST_PASSWORD =' //rhdc2owjl1vpvt3mhv4'
